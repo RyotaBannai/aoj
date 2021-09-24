44a725d061a98a7d11953fa47ef1f895703eb9ae
RyotaBannai <ryotala0528@gmail.com>
2021-09-25 03:28:28 +0900

    refactor: bsort from rabbisland's ans

--- haskell/src/Algo.hs
+++ haskell/src/Algo.hs
@@ -4,6 +4,7 @@ module Algo where
 
 import Common (randomList, readInputContest, readInputsTerm, safeSwapElems)
 import Control.Applicative
+import Data.Bifunctor
 import Data.List
 import Data.Maybe
 import System.IO
@@ -50,12 +51,6 @@ bubbleSort = outer 0 0
          in inner i (j -1) (times + 1) True xs'
       | otherwise = inner i (j -1) times flag xs
 
--- test :: IO ()
--- test = do
---   xs <- randomList 100
---   let (time, xs') = bubbleSort xs
---   putStrLn $ "times: " ++ show time ++ ", sorted list: " ++ show xs'
-
 solve3 :: IO ()
 solve3 = do
   _ <- getLine
@@ -65,14 +60,47 @@ solve3 = do
   print time
 
 {-
+rabbisland's ans| <https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=1460503#1>
+
+by using recursive calls for tail values,
+useful particularly when the returned tail values doesn't fit to tail elems to concatinate with head elem.
+to utilize this technique, use `where` to break up those tail values first.
+-}
+
+bsort :: Ord a => [a] -> (Int, [a])
+bsort xs = outer (0, reverse xs)
+  where
+    outer :: (Ord a) => (Int, [a]) -> (Int, [a])
+    outer (c, []) = (c, [])
+    outer (c, xs) = (nnc, y : zs)
+      where
+        (nc, y : ys) = inner (c, xs) -- (time of sort, [1,5,3,2,4])
+        (nnc, zs) = outer (nc, ys)
+
+    inner :: (Ord a) => (Int, [a]) -> (Int, [a]) -- push small value to the right-most
+    inner (c, [x]) = (c, [x])
+    inner (c, x : xs)
+      | x < y = (nc + 1, y : x : ys)
+      | otherwise = (nc, x : y : ys)
+      where
+        (nc, y : ys) = inner (c, xs)
+
+solve3' :: IO ()
+solve3' = do
+  _ <- getLine
+  xs <- map read . words <$> getLine :: IO [Int]
+  mapM_ putStrLn $ let (c, xs') = bsort xs in [unwords . map show . reverse $ xs', show c]
+
+{-
 napo's ans | <https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=2411742#1>
 
 just sort with default impl
 and the number of sort is calculated just by finding minimun elem index and sum those indices.
 
 -}
-solve3' :: IO ()
-solve3' = getContents >>= putStr . format . (\xs -> (solve' ([], xs), sort xs)) . map read . words . (!! 1) . lines
+
+solve3'' :: IO ()
+solve3'' = getContents >>= putStr . format . (\xs -> (solve' ([], xs), sort xs)) . map read . words . (!! 1) . lines
 
 format :: (([Int], [a]), [Int]) -> String
 format ((ns, _), xs) = unlines [unwords $map show xs, show $ sum ns]
