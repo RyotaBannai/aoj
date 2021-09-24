298de9d2984ce80423a1ba215dc65e9c3ddeca29
RyotaBannai <ryotala0528@gmail.com>
2021-09-25 00:33:11 +0900

    add: bubble sort.

--- haskell/src/Algo.hs
+++ haskell/src/Algo.hs
@@ -2,21 +2,23 @@
 
 module Algo where
 
-import Common (readInputsTerm)
+import Common (randomList, readInputContest, readInputsTerm, safeSwapElems)
 import Control.Applicative
+import Data.List
+import Data.Maybe
 import System.IO
 
 -- ITP1_1_C: Rectangle
-solve3 :: IO ()
-solve3 = do
+solve :: IO ()
+solve = do
   [a, b] <- map read . words <$> getLine
   let area = a * b
       rect = a + 2 + b + 2
   print $ show area ++ " " ++ show rect
 
 -- ALDS1_1_D
-solve4 :: IO ()
-solve4 = do
+solve2 :: IO ()
+solve2 = do
   _ <- getLine
   xs <- fmap read <$> readInputsTerm -- or use `readInputsTerm` for debug
   print $ run xs Nothing Nothing
@@ -31,3 +33,50 @@ solve4 = do
     run (v : rest) maxv Nothing = run rest maxv (Just v)
     run (v : rest) Nothing (Just x) = run rest (Just $ prof x v) (Just (x `min` v))
     run (v : rest) (Just v') (Just x) = run rest (Just $ prof x v `max` v') (Just (x `min` v))
+
+bubbleSort :: Ord a => [a] -> (Int, [a])
+bubbleSort = outer 0 0
+  where
+    outer :: (Ord a) => Int -> Int -> [a] -> (Int, [a])
+    outer i time xs =
+      let (times', flag', xs') = inner i (length xs - 1) time False xs
+       in if not flag' then (times', xs') else outer (i + 1) times' xs'
+
+    inner :: (Ord a) => Int -> Int -> Int -> Bool -> [a] -> (Int, Bool, [a])
+    inner i j times flag xs
+      | j <= i = (times, flag, xs)
+      | (xs !! j) < (xs !! (j -1)) =
+        let xs' = fromJust $ safeSwapElems j (j -1) xs
+         in inner i (j -1) (times + 1) True xs'
+      | otherwise = inner i (j -1) times flag xs
+
+-- test :: IO ()
+-- test = do
+--   xs <- randomList 100
+--   let (time, xs') = bubbleSort xs
+--   putStrLn $ "times: " ++ show time ++ ", sorted list: " ++ show xs'
+
+solve3 :: IO ()
+solve3 = do
+  _ <- getLine
+  xs <- map read . words <$> getLine
+  let (time, xs') = bubbleSort xs :: (Int, [Int])
+  putStrLn $ unwords . map show $ xs'
+  print time
+
+{-
+napo's ans | <https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=2411742#1>
+
+just sort with default impl
+and the number of sort is calculated just by finding minimun elem index and sum those indices.
+
+-}
+solve3' :: IO ()
+solve3' = getContents >>= putStr . format . (\xs -> (solve' ([], xs), sort xs)) . map read . words . (!! 1) . lines
+
+format :: (([Int], [a]), [Int]) -> String
+format ((ns, _), xs) = unlines [unwords $map show xs, show $ sum ns]
+
+solve' :: ([Int], [Int]) -> ([Int], [Int])
+solve' (n, []) = (n, [])
+solve' (n, xs) = let s = minimum xs in solve' (fromJust (elemIndex s xs) : n, delete s xs)
--- haskell/src/Common.hs
+++ haskell/src/Common.hs
@@ -1,5 +1,7 @@
 module Common where
 
+import System.Random
+
 {-
 takeWhileM <https://stackoverflow.com/questions/12119420/haskell-ghci-using-eof-character-on-stdin-with-getcontents>
 -}
@@ -17,3 +19,24 @@ readInputContest = lines <$> getContents
 
 readInputsTerm :: IO [String]
 readInputsTerm = takeWhileM (not . null) (repeat getLine)
+
+-- λ safeSwapElems 1 9 [0..10] # Just [0,9,2,3,4,5,6,7,8,1,10]
+safeSwapElems :: Int -> Int -> [a] -> Maybe [a]
+safeSwapElems f' s' xs
+  | f' == s' = Just xs
+  | f' < 0 || s' < 0 || f' > length xs || s' > length xs = Nothing
+  | otherwise =
+    let (f, s) = if f' < s' then (f', s') else (s', f')
+        elf = xs !! f
+        els = xs !! s
+        left = take f xs
+        middle = take (s - f - 1) (drop (f + 1) xs)
+        right = drop (s + 1) xs
+     in Just $ left ++ els : middle ++ elf : right
+
+randomList :: Int -> IO [Int]
+randomList 0 = return []
+randomList n = do
+  r <- randomRIO (1, 6)
+  rs <- randomList (n -1)
+  return (r : rs)
