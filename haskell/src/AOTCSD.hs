9aaa6a0f7703e2960e36e4e86da60ef40af76063
RyotaBannai <ryotala0528@gmail.com>
2021-09-27 20:02:39 +0900

    add: ALDS1_3_D

--- haskell/src/AOTCSD.hs
+++ haskell/src/AOTCSD.hs
@@ -1,47 +1,58 @@
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module AOTCSD where
+
+type State = (Stack, Areas, Pos)
 
 type Stack = [Int]
 
 type Areas = [(Int, Int)]
 
-type State = (Stack, Areas, Pos)
-
 type Pos = Int
 
-type Accm = (Int, Int)
-
 run :: String -> State -> Areas
-run [] (_, a, _) = a
-run inp@(x : xs) st@(sk, a, i)
-  | x == '/' =
-    let (xs', (sk', a', i'), (l, total)) = go inp st (i, 0)
-     in run xs' (sk', (l, total) : a', i')
-  | x == '\\' = run xs (i : sk, a, i + 1)
-  | otherwise = run xs (sk, a, i + 1)
+run [] (_, as, _) = as
+run (x : xs) (stk, as, i)
+  | x == '\\' = run xs (i : stk, as, i + 1)
+  | x == '/' = case stk of
+    (j : stk') ->
+      let (as', as'') = span ((j <) . fst) as -- 反対側の岸との間にある池.
+          s = i - j
+          ss = sum $ map snd as'
+       in run xs (stk', (j, s + ss) : as'', i + 1) -- 反対側の岸を大きい池の最左点とする.
+    [] -> skip
+  | otherwise = skip
   where
-    go :: String -> State -> Accm -> (String, State, Accm)
-    go inp@(x : xs) ([], as, i) accm = (if x /= '/' then inp else xs, ([], as, if x /= '/' then i -1 else i), accm)
-    go [] st accm = ([], st, accm)
-    go inp@(x : xs) (sk, as, i) accm =
-      if x /= '/'
-        then (inp, (sk, as, if x /= '/' then i -1 else i), accm) -- return without consume `inp`
-        else
-          let pos = head sk
-              dist = (-) i pos
-              area = if (not . null $ as) && (fst . head $ as) > i then head as else (pos, 0)
-              as' = if snd area /= 0 || null as then as else tail as
-              total = dist + snd accm + snd area
-           in go xs (tail sk, as', i + 1) (fst area, total)
+    skip = run xs (stk, as, i + 1)
 
-test_inp :: [Char]
-test_inp = ['\\', '\\', '/', '/']
-
-test_inp2 :: [Char]
-test_inp2 = ['\\', '\\', '/', '/', '/', '\\', '_', '/', '\\', '/', '\\', '\\', '\\', '\\', '/', '_', '/', '\\', '\\', '/', '/', '/', '_', '_', '\\', '\\', '\\', '_', '\\', '\\', '/', '_', '\\', '/', '_', '/', '\\']
+format :: Areas -> [String]
+format (reverse -> as) = let as' = map snd as in [show . sum $ as', unwords . map show $ length as' : as']
 
 main :: IO ()
-main = do
-  xs <- getLine
-  print $ run test_inp2 ([], [], 1)
+main = getLine >>= (\x -> mapM_ putStrLn $ format $ run x ([], [], 0))
+
+{-
+rabbisland's ans with foldl instead of recursion
+
+import Control.Applicative ((<$>))
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as B
+
+main :: IO ()
+main = solve <$> B.getLine >>= mapM_ putStrLn
+
+solve :: ByteString -> [String]
+solve = g . B.foldl' f (0, [], [])
+  where f :: (Int, [Int], [(Int, Int)]) -> Char -> (Int, [Int], [(Int, Int)])
+        f (i, st1, st2) x | x == '\\' = (i+1, i:st1, st2)
+                          | x == '/' = case st1 of
+                                        (j:st1') -> let s = i - j
+                                                        (st2', st2'') = span ((j<) . fst) st2
+                                                    in (i+1, st1', (j, (s + (sum . map snd) st2')):st2'')
+                                        [] -> (i+1, st1, st2)
+                          | otherwise = (i+1, st1, st2)
+        g (_, _, xs) = let ys = map snd xs
+                       in [(show . sum) ys, (unwords . map show . ((length ys):) . reverse) ys]
+
+-}
