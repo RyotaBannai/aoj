be94f33311182edc337f236a71dfb633ff86ca7a
RyotaBannai <ryotala0528@gmail.com>
2021-09-27 23:51:50 +0900

    add: ALDS1_4_B

---
+++ haskell/src/BSearch.hs
@@ -0,0 +1,30 @@
+module BSearch where
+
+import Data.Vector (Vector, (!))
+import qualified Data.Vector as V
+
+type S = Vector Int
+
+type Right = Int
+
+type Left = Int
+
+type Target = Int
+
+search :: S -> Left -> Right -> Target -> Int
+search s l r t
+  | (r - l) <= 1 = 0
+  | s ! m == t = 1
+  | s ! m > t = search s l m t
+  | s ! m < t = search s m r t
+  where
+    m = (l + r) `div` 2
+
+readInt :: IO Int
+readInt = (read :: String -> Int) <$> getLine
+
+readInts :: IO (Vector Int)
+readInts = V.fromList . map (read :: String -> Int) . words <$> getLine
+
+main :: IO ()
+main = (\n s _ t -> V.map (search s 0 n) t) <$> readInt <*> readInts <*> getLine <*> readInts >>= print . V.sum
