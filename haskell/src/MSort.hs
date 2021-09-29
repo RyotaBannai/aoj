5f812d3f832466025fc81dd850f5b6a97529467b
RyotaBannai <ryotala0528@gmail.com>
2021-09-29 23:55:36 +0900

    add: merge sort wip

---
+++ haskell/src/MSort.hs
@@ -0,0 +1,21 @@
+module MSort where
+
+halve :: [a] -> ([a], [a])
+halve [] = ([], [])
+halve xs = let mid = (`div` 2) . length $ xs in splitAt mid xs
+
+-- 1. singleton list どうしの比較であれば確実にソート + マージ可能
+-- 2. ソート済みの 2 つのリストであればマージ + ソート可能
+-- merge [1,6,2] [9,5,1] # [1,6,2,9,5,1]
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x : xs) (y : ys)
+  | x < y = x : merge xs (y : ys)
+  | otherwise = y : merge (x : xs) ys
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = let (left, right) = halve xs in merge (msort left) (msort right)
