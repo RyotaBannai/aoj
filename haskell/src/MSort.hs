97d058a05dcc6e6c9eda315218dd0d76fc338acf
RyotaBannai <ryotala0528@gmail.com>
2021-10-01 01:51:22 +0900

    add: ALDS1_5_B - wrong count number.

--- haskell/src/MSort.hs
+++ haskell/src/MSort.hs
@@ -1,24 +1,29 @@
+{-# LANGUAGE TupleSections #-}
+
 module MSort where
+
+merge :: Ord a => ([a], [a], Int) -> ([a], Int)
+merge (xs, [], cnt) = (xs, cnt + 1)
+merge ([], ys, cnt) = (ys, cnt + 1)
+merge (x : xs, y : ys, cnt)
+  | x < y = (x : merged1, cnt1' + 1)
+  | otherwise = (y : merged2, cnt2' + 1)
+  where
+    (merged1, cnt1') = merge (xs, y : ys, cnt)
+    (merged2, cnt2') = merge (x : xs, ys, cnt)
+
+msort :: Ord a => ([a], Int) -> ([a], Int)
+msort ([], cnt) = ([], cnt)
+msort ([x], cnt) = ([x], cnt)
+msort (xs, cnt) =
+  let (left, right) = halve xs
+      (ls, cnt1) = msort (left, 0)
+      (rs, cnt2) = msort (right, 0)
+   in merge (ls, rs, cnt + cnt1 + cnt2)
 
 halve :: [a] -> ([a], [a])
 halve [] = ([], [])
 halve xs = let mid = (`div` 2) . length $ xs in splitAt mid xs
-
--- 1. singleton list どうしの比較であれば確実にソート + マージ可能
--- 2. ソート済みの 2 つのリストであればマージ + ソート可能
--- merge [1,6,2] [9,5,1] # [1,6,2,9,5,1]
-
-merge :: Ord a => [a] -> [a] -> [a]
-merge xs [] = xs
-merge [] ys = ys
-merge (x : xs) (y : ys)
-  | x < y = x : merge xs (y : ys)
-  | otherwise = y : merge (x : xs) ys
-
-msort :: Ord a => [a] -> [a]
-msort [] = []
-msort [x] = [x]
-msort xs = let (left, right) = halve xs in merge (msort left) (msort right)
 
 readInts :: IO [Int]
 readInts = map read . words <$> getLine
@@ -27,7 +32,4 @@ format :: ([Int], Int) -> [String]
 format (xs, cnt) = [unwords . map show $ xs, show cnt]
 
 main :: IO ()
-main = do
-  _ <- getLine
-  xs <- readInts
-  mapM_ putStrLn $ format . msort xs
+main = (getLine >> readInts) >>= mapM_ putStrLn . format . msort . (,0)
