2388839aaf0a3ce19a8105fc7746fd344bb14dd9
RyotaBannai <ryotala0528@gmail.com>
2021-10-02 19:35:55 +0900

    add: ALDS1_6_C qsort solution

--- cpp/src/qsort.cpp
+++ cpp/src/qsort.cpp
@@ -18,7 +18,7 @@ void print_vector_int(const vector<Card> v)
   cout << endl;
 }
 
-int m = 6;
+int m = 100000;
 vector<Card> vec_l(m / 2 + 2), vec_r(m / 2 + 2);
 
 void merge(vector<Card> &vec, const int l, const int mid, const int r)
@@ -28,9 +28,9 @@ void merge(vector<Card> &vec, const int l, const int mid, const int r)
   int n2 = r - mid;
 
   for (i = 0; i < n1; i++)
-    vec_l[i] = vec[l + 1];
+    vec_l[i] = vec[l + i];
   for (j = 0; j < n2; j++)
-    vec_r[j] = vec[mid + 1];
+    vec_r[j] = vec[mid + j];
 
   // vec_l, vec_r の末尾にどの数値よりも大きな番兵を置くことで簡略化する
   // r-l 回だけループするようにすれば、実際に番兵どうしが比較されることも、i, j が n1, n2
@@ -110,22 +110,22 @@ auto main() -> int
   qsort(vec_a, 0, n - 1);
   msort(vec_b, 0, n);
 
-  print_vector_int(vec_a);
-  print_vector_int(vec_b);
+  // print_vector_int(vec_a);
+  // print_vector_int(vec_b);
 
-  // for (i = 0; i < n; i++) {
-  //   if (vec_a[i].suit != vec_b[i].suit)
-  //     stable = 0;
-  // }
+  for (i = 0; i < n; i++) {
+    if (vec_a[i].suit != vec_b[i].suit)
+      stable = 0;
+  }
 
-  // if (stable == 1)
-  //   printf("Stable\n");
-  // else
-  //   printf("Not stable\n");
+  if (stable == 1)
+    printf("Stable\n");
+  else
+    printf("Not stable\n");
 
-  // for (auto x : vec_b) {
-  //   printf("%c %d\n", x.suit, x.value);
-  // }
+  for (auto x : vec_a) {
+    printf("%c %d\n", x.suit, x.value);
+  }
 
   return 0;
 }
