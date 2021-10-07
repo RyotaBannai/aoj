ae2dfb4ff8cfa9ee1f6fcc9b84b16baa7981b0c0
RyotaBannai <ryotala0528@gmail.com>
2021-10-08 02:04:04 +0900

    add: ALDS1_8_C

--- cpp/src/tree/binary_tree/binary_tree.cpp
+++ cpp/src/tree/binary_tree/binary_tree.cpp
@@ -13,19 +13,18 @@ Node *root, *NIL;
 
 template <typename T> auto in() -> T
 {
-  int inp;
+  T inp;
   cin >> inp;
   return inp;
 }
+
+auto allocNode() -> Node * { return (Node *)malloc(sizeof(Node)); }
 
 void insert(int v)
 {
   Node *y = NIL;
   Node *x = root;
-  Node *z;
-
-  // create new Node
-  z = (Node *)malloc(sizeof(Node));
+  Node *z = allocNode(); // create new Node
   z->key = v;
   z->left = NIL;
   z->right = NIL;
@@ -44,6 +43,62 @@ void insert(int v)
     else
       y->right = z;
   }
+}
+
+auto treeMin(Node *u) -> Node *
+{
+  while (u->left != NIL)
+    u = u->left;
+  return u;
+}
+
+auto treeSuccessor(Node *u) -> Node *
+{
+  if (u->right != NIL)
+    return treeMin(u->right);
+  else {
+    Node *y = u->parent;
+    while (y != NIL && u == y->right) {
+      u = y;         // right かどうかチェックのため
+      y = y->parent; // 上へ登る
+    }
+    return y;
+  }
+}
+
+void deleteNode(Node *u)
+{
+  Node *tar;      // 削除対象
+  Node *sub_tree; // y の子
+
+  if (u->left == NIL || u->right == NIL)
+    // if both or either of children are/is NIL
+    // 末端の Node ならそれを削除、子が１つの時は、それ自体を削除し、削除した場所に子を置く
+    tar = u;
+  else
+    // 子が二つある場合は、中順巡回で次になる子を削除対象とする
+    // 中順巡回で次のノードを探した時に、必ずそのノードは左側のノードを持つことはない（持っている場合、そのノードが次のノードになるため）
+    tar = treeSuccessor(u);
+
+  // 上記の else のロジックの通り、左側に sub tree があるケースが無いため必然的に right
+  // が次の子となる
+  sub_tree = tar->left != NIL ? tar->left : tar->right;
+  if (sub_tree != NIL)
+    sub_tree->parent = tar->parent;
+
+  if (tar->parent == NIL)
+    // 削除対象が root の場合
+    root = sub_tree;
+  else {
+    if (tar == tar->parent->left)
+      tar->parent->left = sub_tree;
+    else
+      tar->parent->right = sub_tree;
+  }
+
+  if (tar != u)
+    u->key = tar->key;
+  free(tar);
 }
 
 auto find(Node *u, int v) -> Node *
@@ -73,7 +128,7 @@ void preorder(Node *u)
 
 auto main() -> int
 {
-  int n, i, x;
+  int n, i;
   string com;
 
   cin >> n;
@@ -90,6 +145,8 @@ auto main() -> int
     }
     else if (com[0] == 'f')
       printf("%s\n", find(root, in<int>()) != NIL ? "yes" : "no");
+    else if (com[0] == 'd')
+      deleteNode(find(root, in<int>()));
   }
 
   return 0;
