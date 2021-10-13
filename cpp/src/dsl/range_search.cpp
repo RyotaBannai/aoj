c756cf88c7188382df6bea83052c0f3e3b2d2dd9
RyotaBannai <ryotala0528@gmail.com>
2021-10-13 20:07:15 +0900

    add: DSL_2_C kD tree

--- cpp/src/dsl/range_search.cpp
+++ cpp/src/dsl/range_search.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
+#include <cstdio>
 #include <iostream>
 #include <vector>
 #define lp(i, n) for (int i = 0; i < n; i++)
+#define node t[v]
 using namespace std;
 
 class Node {
@@ -14,7 +17,7 @@ public:
   int id, x, y;
   Point() = default;
   Point(int id, int x, int y) : id(id), x(x), y(y) {}
-  auto operator<(const Point &p) -> const bool { return id < p.id; }
+  auto operator<(const Point &p) const -> bool { return id < p.id; }
   void print() { printf("%d\n", id); }
 };
 
@@ -28,48 +31,48 @@ Node t[MAX];
 auto less_x(const Point &p1, const Point &p2) -> bool { return p1.x < p2.x; }
 auto less_y(const Point &p1, const Point &p2) -> bool { return p1.y < p2.y; }
 
+auto even(int d) -> bool { return d % 2 == 0; };
+
 auto make_kd_tree(int l, int r, int depth) -> int
 {
   if (!(l < r))
     return NIL;
-  sort(points + l, points + r, depth % 2 == 0 ? less_x : less_y);
 
+  sort(points + l, points + r, even(depth) ? less_x : less_y);
   int mid = (l + r) / 2;
   int v = np++;
-  t[v].location = mid;
-  t[v].left = make_kd_tree(l, mid, depth + 1);
-  t[v].right = make_kd_tree(mid + 1, r, depth + 1);
+  node.location = mid;
+  node.left = make_kd_tree(l, mid, depth + 1);
+  node.right = make_kd_tree(mid + 1, r, depth + 1);
 
   return v;
 }
 
 void find(int v, int sx, int tx, int sy, int ty, int depth, vector<Point> &ans)
 {
-  int x = points[t[v].location].x;
-  int y = points[t[v].location].y;
+  int x = points[node.location].x;
+  int y = points[node.location].y;
 
   if (sx <= x && x <= tx && sy <= y && y <= ty)
-    ans.push_back(points[t[v].location]);
+    ans.push_back(points[node.location]);
 
-  if (depth % 2 == 0) {
-    if (t[v].left != NIL)
-      if (sx <= x)
-        find(t[v].left, sx, tx, sy, ty, depth + 1, ans);
-    if (t[v].right != NIL)
-      if (x <= tx)
-        find(t[v].right, sx, tx, sy, ty, depth + 1, ans);
+  auto apply = [&](int idx) { find(idx, sx, tx, sy, ty, depth + 1, ans); };
+
+  if (even(depth)) {
+    if (node.left != NIL && sx <= x)
+      apply(node.left);
+    if (node.right != NIL && x <= tx)
+      apply(node.right);
   }
   else {
-    if (t[v].left != NIL)
-      if (sy <= y)
-        find(t[v].left, sx, tx, sy, ty, depth + 1, ans);
-    if (t[v].right != NIL)
-      if (y <= ty)
-        find(t[v].right, sx, tx, sy, ty, depth + 1, ans);
+    if (node.left != NIL && sy <= y)
+      apply(node.left);
+    if (node.right != NIL && y <= ty)
+      apply(node.right);
   }
 }
 
-auto main() -> int
+void create_points()
 {
   int x, y;
   scanf("%d", &n);
@@ -79,22 +82,31 @@ auto main() -> int
     points[i] = Point(i, x, y);
     t[i].left = t[i].right = t[i].parent = NIL;
   }
+}
 
-  np = 0;
-  int root = make_kd_tree(0, n, 0);
-  int q;
+void solve(int root)
+{
+  int q, sx, tx, sy, ty;
   scanf("%d", &q);
-  int sx, tx, sy, ty;
   vector<Point> ans;
-  lp(i, 1)
+  lp(i, q)
   {
     scanf("%d %d %d %d", &sx, &tx, &sy, &ty);
     ans.clear();
     find(root, sx, tx, sy, ty, 0, ans);
+    sort(ans.begin(), ans.end());
     for (auto &a : ans)
       a.print();
     printf("\n");
   }
+}
+
+auto main() -> int
+{
+  create_points();
+
+  np = 0;
+  solve(make_kd_tree(0, n, 0));
 
   return 0;
 }
